822643297334d53ab8afc3ae4cca46b42591b12e
jwthompson93 <shadow17993@gmail.com>
2023-10-25 17:41:04 +0100

    Split the Validation and Increment tests into separate classes. Added more tests to the Increment tests

---
+++ VersionNumberIncrementerTests/Tests/IncrementVersionTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VersionNumberIncrementer.Objects;
+
+namespace VersionNumberIncrementerTests.Tests
+{
+    [TestClass]
+    public class IncrementVersionTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 5, 39, 8, 0 }, "5.39.8.0", 3, 4)]
+        public void TestGetFormattedVersionNumber(int[] versionNumberArray,
+            string expectedResult, int major_release_position, int minor_release_position)
+        {
+            VersionNumber versionNumber = new VersionNumber(versionNumberArray,
+                major_release_position, minor_release_position);
+            Assert.AreEqual(expectedResult, versionNumber.getFormattedVersionNumber('.'));
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 5, 39, 8, 0 }, "5.39.9.0", 3, 4)]
+        [DataRow(new int[] { 5, 39, 8, 2 }, "5.39.9.0", 3, 4)]
+        [DataRow(new int[] { 5, 39, 8, 2 }, "5.40.0.0", 2, 3)]
+        [DataRow(new int[] { 5, 39, 8, 2 }, "6.0.0.0", 1, 2)]
+        [DataRow(new int[] { 5, 39, 8 }, "5.40.0", 2, 3)]
+        [DataRow(new int[] { 5, 39, 8 }, "6.0.0", 1, 2)]
+        [DataRow(new int[] { 5, 39 }, "6.0", 1, 2)]
+        public void TestVersionNumberIncrementMajorVersion(int[] versionNumberArray,
+            string expectedResult, int major_release_position, int minor_release_position)
+        {
+            VersionNumber versionNumber = new VersionNumber(versionNumberArray,
+                major_release_position, minor_release_position);
+            versionNumber.incrementMajorVersion();
+            Assert.AreEqual(expectedResult, versionNumber.getFormattedVersionNumber('.'));
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 5, 39, 8, 0 }, "5.39.8.1", 3, 4)]
+        [DataRow(new int[] { 5, 39, 8, 2 }, "5.39.8.3", 3, 4)]
+        [DataRow(new int[] { 5, 39, 8, 2 }, "5.39.9.0", 2, 3)]
+        [DataRow(new int[] { 5, 39, 8, 2 }, "5.40.0.0", 1, 2)]
+        [DataRow(new int[] { 5, 39, 8 }, "5.39.9", 2, 3)]
+        [DataRow(new int[] { 5, 39, 8 }, "5.40.0", 1, 2)]
+        [DataRow(new int[] { 5, 39 }, "5.40", 1, 2)]
+        public void TestVersionNumberIncrementMinorVersion(int[] versionNumberArray,
+            string expectedResult, int major_release_position, int minor_release_position)
+        {
+            VersionNumber versionNumber = new VersionNumber(versionNumberArray,
+                major_release_position, minor_release_position);
+            versionNumber.incrementMinorVersion();
+            Assert.AreEqual(expectedResult, versionNumber.getFormattedVersionNumber('.'));
+        }
+    }
+}
---
+++ VersionNumberIncrementerTests/Tests/ValidationTests.cs
@@ -0,0 +1,69 @@
+﻿using VersionNumberIncrementer.Objects;
+using VersionNumberIncrementer.Validation;
+
+namespace VersionNumberIncrementerTests.Tests
+{
+    [TestClass]
+    public class ValidationTests
+    {
+        
+
+        [TestMethod]
+        [DataRow(new int[] { 5, 39, 8, 0 }, 3, 4)]
+        public void TestValidationPasses(int[] versionNumberArray, 
+            int major_release_position, int minor_release_position)
+        {
+            VersionNumber versionNumber = new VersionNumber(versionNumberArray, 
+                major_release_position, minor_release_position);
+            Assert.IsTrue(new VersionNumberValidator().Validate(versionNumber));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException),
+            "Validates that at least one of the release position numbers are out of range")]
+        [DataRow(new int[] { 5, 39, 8, 0 }, 4, 5)]
+        public void TestValidationIsFalseIndexOutOfRange(int[] versionNumberArray, 
+            int major_release_position, int minor_release_position)
+        {
+            VersionNumber versionNumber = new VersionNumber(versionNumberArray, 
+                major_release_position, minor_release_position);
+            new VersionNumberValidator().Validate(versionNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception),
+            "Throws Exception since Major and Minor Number Positions are the same")]
+        [DataRow(new int[] { 5, 39, 8, 0 }, 3, 3)]
+        public void TestValidationMajorMinorNumberIsSameException(int[] versionNumberArray, 
+            int major_release_position, int minor_release_position)
+        {
+            VersionNumber versionNumber = new VersionNumber(versionNumberArray, 
+                major_release_position, minor_release_position);
+            new VersionNumberValidator().Validate(versionNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception),
+            "Throws Exception since Minor Number Position is lower than the Major Position")]
+        [DataRow(new int[] { 5, 39, 8, 0 }, 3, 2)]
+        public void TestValidationMinorNumberLowerThanMajorException(int[] versionNumberArray, 
+            int major_release_position, int minor_release_position)
+        {
+            VersionNumber versionNumber = new VersionNumber(versionNumberArray, 
+                major_release_position, minor_release_position);
+            new VersionNumberValidator().Validate(versionNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception),
+            "Throws Exception since release number is negative")]
+        [DataRow(new int[] { 5, -39, 8, 0 }, 3, 2)]
+        public void TestValidationReleaseNumberIsNegativeException(int[] versionNumberArray, 
+            int major_release_position, int minor_release_position)
+        {
+            VersionNumber versionNumber = new VersionNumber(versionNumberArray, 
+                major_release_position, minor_release_position);
+            new VersionNumberValidator().Validate(versionNumber);
+        }
+    }
+}
